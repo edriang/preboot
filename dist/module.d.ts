50fa645ae0a4db74352d37a31f791bf7637aa0ff
Adrián Gallardo <adrian.gallardo@wizeline.com>
2021-07-01 12:28:36 -0500

    chore: updated version

--- dist/module.d.ts
+++ dist/module.d.ts
@@ -7,10 +7,6 @@
  */
 import { ModuleWithProviders } from '@angular/core';
 import { PrebootOptions } from './common/preboot.interfaces';
-import * as i0 from "@angular/core";
 export declare class PrebootModule {
     static withConfig(opts: PrebootOptions): ModuleWithProviders<PrebootModule>;
-    static ɵfac: i0.ɵɵFactoryDef<PrebootModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<PrebootModule, never, never, never>;
-    static ɵinj: i0.ɵɵInjectorDef<PrebootModule>;
 }
